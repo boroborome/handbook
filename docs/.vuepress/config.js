2649075e652b93eb18921f48d68b48e8f1b435f5
Yushan Gao <ysgao@thoughtworks.com>
2021-01-03 22:42:08 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: 'Hello Happy3w',
+    description: 'Just playing around',
+    markdown: {
+        lineNumbers: true,
+        toc: {
+            includeLevel: [1, 2, 3],
+        },
+    },
+    themeConfig: {
+        nav: [
+            {
+                text: "手册",
+                link: "/handbook/",
+            },
+        ],
+        sidebar: [
+            '/handbook/',
+            '/handbook/toolkits/',
+            '/handbook/persistence-core/',
+            '/handbook/persistence-jpa/',
+            '/handbook/persistence-es/',
+            '/handbook/persistence-excel/',
+        ],
+        sidebarDepth: 3,
+    },
+}
---
+++ docs/handbook/persistence-jpa/READMe.md
@@ -0,0 +1,43 @@
+---
+title: persistence-jpa
+---
+
+# persistence-jpa
+
+
+Quick Start
+-----------
+## Maven/Gradle configuration
+
+Add the Maven dependency:
+
+```xml
+<dependency>
+    <groupId>com.happy3w</groupId>
+    <artifactId>persistence-jpa</artifactId>
+    <version>0.0.1</version>
+</dependency>
+```
+
+Add the Gradle dependency:
+
+```groovy
+implementation 'com.happy3w:persistence-jpa:0.0.1'
+```
+
+## 组件介绍
+- JpaAssistant
+
+---
+
+
+
+### JpaAssistant
+
+
+
+
+
+## 历史
+
+- 0.0.1 实现基本的JpaAssistant
